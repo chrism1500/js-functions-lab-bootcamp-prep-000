7b587bc00f18658ce8232aebbd2bf3267d811ce4
morales.chris1989@gmail.com <morales.chris1989@gmail.com>
2018-07-11 20:52:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ function happyHolidays() {
   
 }
 
-function happyHolidaysTo(name){
-  return 'Happy Holidays, $(name)';
-  
-}
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`;
+ };
